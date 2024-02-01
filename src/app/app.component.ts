fa065ec6716f66663fba30ad41fed79f90a0f77b
Ali <Alipro.ig7@gmail.com>
2024-02-01 14:26:27 +0300

    pipes app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pipes';
+  nameChange: string = '';
+  date: string = '';
+  amount: number | any;
+  hight: number = 0
+
+  car = {
+    make: "Toyota",
+    model: 'camry', 
+    year: 2000
+  }
+
+  miles : number = 0;
 }
